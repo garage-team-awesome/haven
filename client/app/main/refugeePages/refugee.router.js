7ef77e3a60a2dce66c3b1bec4c9739b635e8b818
Eric King <e.king117@gmail.com>
2016-04-03 12:16:28 -0400

    Menu page done

--- client/app/main/refugeePages/refugee.router.js
+++ client/app/main/refugeePages/refugee.router.js
@@ -7,27 +7,27 @@ angular.module('havenApp')
         url: '/home',
         templateUrl: 'app/main/refugeePages/home.template.html',
         controller: 'HomeController',
-        controllerAs: 'vm',
-        authenticate: 'user'
+        controllerAs: 'vm'
+        // authenticate: 'user'
       })
       .state('medical', {
         url: '/medical',
         templateUrl: 'app/main/refugeePages/medical.template.html',
         controller: 'MedicalController',
-        controllerAs: 'vm',
-        authenticate: 'user'
+        controllerAs: 'vm'
+        // authenticate: 'user'
       }).state('people', {
         url: '/people',
         templateUrl: 'app/main/refugeePages/people.template.html',
         controller: 'PeopleController',
-        controllerAs: 'vm',
-        authenticate: 'user'
+        controllerAs: 'vm'
+        // authenticate: 'user'
       })
       .state('person', {
         url: '/person',
         templateUrl: 'app/main/refugeePages/person.template.html',
         controller: 'PersonController',
-        controllerAs: 'vm',
-        authenticate: 'user'
+        controllerAs: 'vm'
+        // authenticate: 'user'
       });
   });
